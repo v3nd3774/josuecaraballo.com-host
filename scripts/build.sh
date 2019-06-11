4d225bbfb799b772cd3b8ffa7a8a7a5b2bab8abb
v3nd3774 <josue.caraballo@gmail.com>
2019-06-11 18:13:23 -0500

    adding docker build arg

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ project_version=`cat project.clj | grep def | perl -ne "print \$1 if /.+\"(.*?)\
 if [ "$1" = "jar" ]; then
   lein uberjar
 elif [ "$1" = "image" ]; then
-  docker build -t v3nd3774/$project:$TRAVIS_BUILD_NUMBER
+  docker build -t v3nd3774/$project:$TRAVIS_BUILD_NUMBER .
 else
   echo "$1 not in (jar|image)..."
   exit 1
