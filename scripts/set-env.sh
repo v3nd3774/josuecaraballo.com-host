6758c1520027354093006e17b0aa324c0d206865
v3nd3774 <josue.caraballo@gmail.com>
2019-06-12 16:01:25 -0500

    ignore pem

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -4,3 +4,6 @@ project_version=`cat project.clj | grep defproject | perl -ne 'print $1 if /.+\"
 docker_tag="$DOCKER_USERNAME/$project:$project_version"
 h_port=80
 c_port=3000
+KEY_PATH=travisci.pem
+aws s3 cp s3://josuecaraballo.com-host/$KEY_PATH .
+chmod 400 $KEY_PATH
