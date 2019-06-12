5364f564c32fc6172811fbc8daf2bb6095c57429
v3nd3774 <josue.caraballo@gmail.com>
2019-06-11 21:21:38 -0500

    go add arg to build image command

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ source scripts/set-env.sh
 if [ "$1" = "jar" ]; then
   lein uberjar
 elif [ "$1" = "image" ]; then
-  docker build -t $docker_tag
+  docker build -t $docker_tag .
 else
   echo "$1 not in (jar|image)..."
   exit 1
