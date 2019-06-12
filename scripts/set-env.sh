3e8b9fa7876f0f6baf63f75bf349526a7f787ae9
v3nd3774 <josue.caraballo@gmail.com>
2019-06-11 21:51:05 -0500

    retry to keep version from adding 0.3.2

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 project=josuecaraballo.com-host
-project_version=`cat project.clj | grep def | perl -ne 'print $1 if /.+\"(.*?)\".+/' -`
+project_version=`cat project.clj | grep defproject | perl -ne 'print $1 if /.+\"(.*?)\".+/' -`
 docker_tag="$DOCKER_USERNAME/$project:$project_version"
