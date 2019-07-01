a32af538b6ece923ca43d77e2b7fd9e34322b0c5
v3nd3774 <josue.caraballo@gmail.com>
2019-07-01 17:07:50 -0500

    updated commit script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,6 +3,6 @@ source scripts/set-env.sh
 source scripts/clean.sh
 git add .
 git status
-read -p 'Commit Message' commitmsg
+read -p 'Commit Message: ' commitmsg
 git add .
 git commit -am "${commitmsg}"
