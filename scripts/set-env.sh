787cd2cf7739eb93be06cec1ca677f02241cef92
v3nd3774 <josue.caraballo@gmail.com>
2019-07-07 12:42:34 -0500

    retry without \.exe ext

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 function site_exec_path () {
-  echo `find . -type f -regex ".*bin.*site\.exe" | head -n 1`
+  echo `find . -type f -regex ".*bin.*site.*" | head -n 1`
 }
 site=josuecaraballo
